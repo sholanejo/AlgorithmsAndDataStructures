b1f2c675af6c15920f89ec0ba5e0615481f4e20b
Shola Nejo <sholanejo@live.com>
2022-01-07 09:51:14 +0100

    Add project files.

---
+++ AlgorithmsAndDataStructures/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsAndDataStructures
+{
+    public static class BinarySearch
+    {
+        public static int BinSearch(int[] sortedArray, int item)
+        {
+            int low = 0;
+            int high = sortedArray.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int guess = (sortedArray[mid]);
+                if(guess == item)
+                {
+                    return mid;
+                }
+                if (guess > item)
+                {
+                    high = mid - 1;
+                }
+                else
+                    low = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures
+{
+    class Program
+    {
+        public static int[] arrTest = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine(BinarySearch.BinSearch(arrTest, 1)); 
+
+            Console.ReadKey();
+        }
+    }
+}
