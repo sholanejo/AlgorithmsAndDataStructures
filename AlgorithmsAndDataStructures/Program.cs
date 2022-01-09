cba6da05797e1bd4dc3fb67c4a9b569b11d9026c
Shola Nejo <sholanejo@live.com>
2022-01-09 20:06:27 +0100

    Recursion

--- AlgorithmsAndDataStructures/Program.cs
+++ AlgorithmsAndDataStructures/Program.cs
@@ -11,13 +11,13 @@ namespace AlgorithmsAndDataStructures
             //Console.WriteLine("The smallest number is " + SelectionSort.SmallestNumber(arrTest));
             //Console.WriteLine("The smallest number is " + SelectionSort.SelectSort(arrTest));
             //Console.WriteLine(BinarySearch.BinSearch(arrTest, 1)); 
+            //int[] arr = new int[10] { 1, 5, 4, 11, 20, 8, 2, 98, 90, 16 };
+            //SelectionSort.Sort(arr);
+            //Console.WriteLine("Sorted Values:");
+            //for (int i = 0; i < arr.Length; i++)
+            //    Console.WriteLine(arr[i]);
 
-
-            int[] arr = new int[10] { 1, 5, 4, 11, 20, 8, 2, 98, 90, 16 };
-            SelectionSort.Sort(arr);
-            Console.WriteLine("Sorted Values:");
-            for (int i = 0; i < arr.Length; i++)
-                Console.WriteLine(arr[i]);
+            Console.WriteLine(Recursion.Recursive(4));
 
             Console.ReadKey();
         }
---
+++ AlgorithmsAndDataStructures/Recursion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsAndDataStructures
+{
+   public static class Recursion
+    {
+        public static int Recursive(int n)
+        {
+            if(n == 0 | n == 1)
+            {
+                return 1;
+            }
+            return n * Recursive(n - 1);
+        }
+    }
+}
